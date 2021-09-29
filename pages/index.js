ba4207b1a496af24c1501c0af32a6ac9041af636
Felix <felixrunye@gmail.com>
2021-09-29 17:04:32 +0300

    updated showing of google image at the center of the screen

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { ViewGridIcon } from '@heroicons/react/solid';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -20,14 +22,19 @@ export default function Home() {
           <p className="link">Gmail</p>
           
           {/* icon */}
+          <ViewGridIcon className="w-5 h-5 p-1 rounded-full hover:bg-gray-300 cursor-pointer"/>
           {/* image */}
-          <div className="p-1 bg-blue-500 rounded-full">
+          <div className="p-1 bg-blue-500 rounded-full cursor-pointer transition duration-150 hover:scale-105 ease-in-out">
             <img loading="lazy" src="https://lh3.googleusercontent.com/a-/AOh14GjkbYYp7lahAZhwF2jCOwBnPjXXYZhGNfuvvi9O8dQ=s96-c"
-            width="35" height="50" className="rounded-full" />
+            width="35" height="50" className="rounded-full " />
           </div>
         </div>
       </header>
       {/* body */}
+      <form>
+        <Image src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+        height={100} width={300} />
+      </form>
 
       {/* footer */}
 
