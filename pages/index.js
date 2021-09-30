0311a3c419da5945e175cb24e47610800be922d7
Felix <felixrunye@gmail.com>
2021-09-30 10:44:03 +0300

    setting up the footer to be grid responsive

--- pages/index.js
+++ pages/index.js
@@ -51,22 +51,24 @@ export default function Home() {
 
       {/* footer */}
         <div className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100
-        text-sm text-gray-500">
+        text-sm text-gray-500 ">
           <div className="px-8 py-3">
-            <p>Kenya</p>
+            <p>  Browsing From Kenya</p>
             <p></p>
           </div>
-          <div className="grid grid-cols-1">
-            <div className="flex justify-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense">
+            <div className="flex justify-center items-center md:col-span-2 
+            lg:col-span-1 lg:col-start-2">
               <GlobeIcon className="h-5 mr-1 text-green-700" />
               <p>Carbon Neurtal Since 2007 </p>
             </div>
-            <div className="flex justify-center space-x-8 whitespace-nowrap">
+            <div className="flex justify-center space-x-8 whitespace-nowrap
+            md:justify-self-start">
               <p>Advertising</p>
               <p>Business</p>
               <p>Now Search Works</p>
             </div>
-            <div className="flex justify-center space-x-8 whitespace-nowrap">
+            <div className="flex justify-center space-x-8 whitespace-nowrap md:ml-auto pr-5">
               <p>Privacy</p>
               <p>Terms</p>
               <p>Settings</p>
