b96e1f126e067b2ba0933bae9bd8c900a9338993
Felix <felixrunye@gmail.com>
2021-10-05 09:27:43 +0300

    null safety

--- pages/SearchResults.js
+++ pages/SearchResults.js
@@ -4,11 +4,11 @@ function SearchResults({results}) {
     return (
         <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
             <p className="text-gray-600 text-md mb-5 mt-3">
-                About {results.searchInformation?.formattedTotalResults} results ( { results.searchInformation.formattedSearchTime} Seconds)
+                About {results?.searchInformation?.formattedTotalResults} results ( { results?.searchInformation?.formattedSearchTime} Seconds)
                 
              </p>
 
-             {results.items?.map((results, index)=>(
+             {results?.items?.map((results, index)=>(
                  <div className="max-w-xl mb-8" key={index}>
                      <div className="group">
                         <a href={results.link} className="text-sm ">{results.formattedUrl} </a>
