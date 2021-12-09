1d5a40b3bfeb85a5e2da6503660b027879c5b974
Felix <felixrunye@gmail.com>
2021-12-09 21:26:51 +0300

    search results update

--- pages/SearchResults.js
+++ pages/SearchResults.js
@@ -1,30 +1,32 @@
-import PaginationButtons from "../components/PaginationButtons"
+import PaginationButtons from "../components/PaginationButtons";
 
-function SearchResults({results}) {
-    return (
-        <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
-            <p className="text-gray-600 text-md mb-5 mt-3">
-                About {results?.searchInformation?.formattedTotalResults} results ( { results?.searchInformation?.formattedSearchTime} Seconds)
-                
-             </p>
+function SearchResults({ results }) {
+  return (
+    <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
+      <p className="text-gray-600 text-md mb-5 mt-3">
+        About {results?.searchInformation?.formattedTotalResults} results ({" "}
+        {results?.searchInformation?.formattedSearchTime} Seconds)
+      </p>
 
-             {results?.items?.map((results, index)=>(
-                 <div className="max-w-xl mb-8" key={index}>
-                     <div className="group">
-                        <a href={results.link} className="text-sm ">{results.formattedUrl} </a>
-                        <a href={results.link}  >
-                            <h2 className="truncate text-blue-800 text-xl font-medium group-hover:underline">
-                                {results.title}
-                            </h2>
-                        </a>
-                     </div>
-                     <p className="line-clamp-2">{results.snippet}</p>
-                 </div>
-             ))}
-
-             <PaginationButtons />
+      {results?.items?.map((results, index) => (
+        <div className="max-w-xl mb-8" key={index}>
+          <div className="group">
+            <a href={results.link} className="text-sm ">
+              {results.formattedUrl}{" "}
+            </a>
+            <a href={results.link}>
+              <h2 className="truncate text-blue-800 text-xl font-medium group-hover:underline">
+                {results.title}
+              </h2>
+            </a>
+          </div>
+          <p className="line-clamp-2">{results.snippet}</p>
         </div>
-    )
+      ))}
+
+      <PaginationButtons />
+    </div>
+  );
 }
 
-export default SearchResults
+export default SearchResults;
